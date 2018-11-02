f4b01cd771bb5c6869c0b479215b4505ac92690c
zantelope <42042206+zantelope@users.noreply.github.com>
2018-11-02 15:53:18 -0500

    Update and rename largestPalindrome.py to first_attempt_largestPalindrome.py

--- first_attempt_largestPalindrome.py
+++ first_attempt_largestPalindrome.py
@@ -1,18 +1,17 @@
-
 def largestPalindrome():
-
+  
   ### initiate variable to store largest palindrom product found
   largest = 0
   
   ### iterate through all 3 digit numbers for 2 sets, i and j
-  for i in range(999):
-    for j in range(999):
+  for i in range(100, 1000):
+    for j in range(100, 1000):
       
       ###determine if product is a palindrome as well as larger than the previous largest value
-      if str((i + 1) * (j + 1))[::-1] == str((i + 1) * (j + 1)) and (i + 1) * (j + 1) > largest:
+      if str(i * j)[::-1] == str(i * j) and i * j > largest:
 
-        ### if both conditions are met, replace largest with current value of (i + 1)(j + 1)
-        largest = (i + 1) * (j + 1)
+        ### if both conditions are met, replace largest with current value of i * j
+        largest = i * j
         
   ### return
   return largest
